af2600ac3714aae203865a9fb3d7dff5dd0e35a5
Florian Lenz <flenz81@googlemail.com>
2017-12-15 13:18:30 +0100

    [tests] system bot - added test's for handleMsg

--- src/system_bot/systemBot.test.js
+++ src/system_bot/systemBot.test.js
@@ -16,6 +16,34 @@ describe('systemBot', () => {
 
         })
 
+    });
+
+    describe('handleMsg', () => {
+
+        test('handle message with existing handler', () => {
+
+            sb.addHandler('profile:create', function () {
+                return new Promise((res, rej) => res('created profile'))
+            });
+
+            return expect(sb.handleMsg('profile:create')).resolves.toBe('created profile');
+
+        });
+
+        test('handle message with handler that doesn\'t exist', () => {
+
+            return expect(sb.handleMsg('command_that_does_not_exist')).resolves.toBe('I couldn\'t process \'command_that_does_not_exist\'. Type \'help\' to list all commands.');
+
+        });
+
+        test('handle message with similar handler name', () => {
+
+            sb.addHandler('profile:create', () => new Promise((res, rej) => res('')));
+            
+            return expect(sb.handleMsg('profile:crate')).resolves.toBe('We couldn\'t process \'profile:crate\'. Did you mean \'profile:create\'');
+
+        })
+
     })
 
 
